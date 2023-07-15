34c6d2fc42be55e2b5ccb6706b072c42e55df5a1
Mark Kovalev <mark.kovalev@sephora.com>
2023-07-15 12:52:46 +0300

    Changes 7/15/2023

---
+++ src/main/java/mayton/libs/encoders/cryptocurrency/Hashes.java
@@ -0,0 +1,19 @@
+package mayton.libs.encoders.cryptocurrency;
+
+public class Hashes {
+
+    public static boolean containsBase64Set(String s) {
+        for (char c : s.toCharArray()) {
+            if (!Character.isDigit(c) || !(c >= 'a' && c <= 'z') || !(c >= 'A' && c <= 'Z') || c!='+' || c!='/' || c !='=') return false;
+        }
+        return true;
+    }
+
+    public static boolean containsBinHexOnly(String s) {
+        for (char c : s.toLowerCase().toCharArray()) {
+            if (!Character.isDigit(c) || !(c >= 'a' && c <= 'f')) return false;
+        }
+        return true;
+    }
+
+}
